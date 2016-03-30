c4181e84148c4fb27a29d4e644a8ad3f1e34b714
nixoninnes <nixoninnes@gmail.com>
2016-03-31 00:17:10 +0100

    added rotating file logger, and main loop

--- app.py
+++ app.py
@@ -2,28 +2,34 @@ import os
 import time
 import logging
 import requests
+from logging.handlers import RotatingFileHandler
 from bs4 import BeautifulSoup
 from slackclient import SlackClient
 
-logging.basicConfig(filename='helpy.log', level=logging.INFO)
-log = logging.getLogger('ex')
+handler = RotatingFileHandler('logs/helpy.logs', maxBytes=100000, backupCount=5)
+logger = logging.getLogger('default')
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 token = os.environ.get('SLACK_TOKEN')
 client = SlackClient(token)
 
-logging.debug(client.api_call('api.test'))
+logger.debug(client.api_call('api.test'))
+
 
 def get_search_url(query):
     query = query.strip().replace(":", "%3A").replace("+", "%2B").replace("&", "%26").replace(" ", "+")
     return "http://www.google.com/search?hl=en&q=site:docs.python.org/3/+{}".format(query)
+
 
 def get_html(url):
     try:
         request = requests.get(url)
         return request.text
     except:
-        logging.error("Error accessing:", url)
+        logger.error("Error accessing:", url)
         return None
+
 
 def search(query):
     url = get_search_url(query)
@@ -31,27 +37,37 @@ def search(query):
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         try:
-            item = soup.find("div", attrs = { "class" : "g" })
+            item = soup.find("div", attrs={"class": "g"})
             link = item.find("cite").text
-            desc = item.find("div", attrs = {"class": "_sPg"}) or item.find("span", attrs = { "class" : "st" })
+            desc = item.find("div", attrs={"class": "_sPg"}) or item.find("span", attrs={"class": "st"})
             desc = desc.text.replace('\n', '').strip()
         except:
             return "Could not find help on _{}_.".format(query), None
     return desc, link
 
-if client.rtm_connect():
-    while True:
-        new_events = client.rtm_read()
-        for event in new_events:
-            if 'type' in event and 'text' in event and 'channel' in event and event['type'] == 'message':
-                logging.info('channel: '+event['channel']+', user:'+event['user']+', msg:'+event['text'])
-                msg = event['text']
-                if msg.split()[0] == 'helpy':
-                    query = ' '.join(msg.split()[1:])
-                    try:
-                        desc, link = search(query)
-                        resp = "{}{}".format(desc, '\n'+link if link is not None else '')
-                        client.rtm_send_message(event['channel'], resp)
-                    except Exception:
-                        log.exception('Error!')
-            time.sleep(1)
+
+def main():
+    if client.rtm_connect():
+        while True:
+            new_events = client.rtm_read()
+            for event in new_events:
+                if 'type' in event and 'text' in event and 'channel' in event and event['type'] == 'message':
+                    logger.info('channel: ' + event['channel'] + ', user:' + event['user'] + ', msg:' + event['text'])
+                    msg = event['text']
+                    if msg.split()[0] == 'helpy':
+                        query = ' '.join(msg.split()[1:])
+                        try:
+                            desc, link = search(query)
+                            resp = "{}{}".format(desc, '\n' + link if link is not None else '')
+                            client.rtm_send_message(event['channel'], resp)
+                        except Exception:
+                            logger.exception('Error!')
+                time.sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        logger.debug("Starting up helpy...")
+        main()
+    except KeyboardInterrupt:
+        logger.debug("Exiting!")
